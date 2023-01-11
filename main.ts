234ea6a971fcbf522319feb9fd8ca46747bb9cea
Conor Gallagher <conorjgallagher@gmail.com>
2023-01-11 21:25:24 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ input.onButtonPressed(Button.A, function () {
     if (GameOver == 1) {
         GameOver = 0
         Score = 0
+        AorB = randint(0, 1)
     } else {
-        if (randint(0, 1) == 1) {
+        if (AorB == 0) {
             pins.digitalWritePin(DigitalPin.P1, 1)
             basic.showIcon(IconNames.Yes)
             basic.pause(500)
@@ -16,6 +17,7 @@ input.onButtonPressed(Button.A, function () {
                 . . . . .
                 . . . . .
                 `)
+            AorB = randint(0, 1)
         } else {
             music.playTone(131, music.beat(BeatFraction.Whole))
             pins.digitalWritePin(DigitalPin.P0, 1)
@@ -29,12 +31,48 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    if (GameOver == 0) {
+        if (AorB == 0) {
+            basic.showLeds(`
+                . . # . .
+                . # . # .
+                # . . . #
+                # # # # #
+                # . . . #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # # # # .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+})
 input.onButtonPressed(Button.B, function () {
     if (GameOver == 1) {
         GameOver = 0
         Score = 0
+        AorB = randint(0, 1)
     } else {
-        if (randint(0, 1) == 1) {
+        if (AorB == 1) {
             pins.digitalWritePin(DigitalPin.P1, 1)
             basic.showIcon(IconNames.Yes)
             basic.pause(500)
@@ -47,6 +85,7 @@ input.onButtonPressed(Button.B, function () {
                 . . . . .
                 . . . . .
                 `)
+            AorB = randint(0, 1)
         } else {
             music.playTone(131, music.beat(BeatFraction.Whole))
             pins.digitalWritePin(DigitalPin.P0, 1)
@@ -61,14 +100,19 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 let TopScore = 0
+let AorB = 0
 let Score = 0
 let GameOver = 0
 GameOver = 1
 Score = 0
 basic.forever(function () {
     while (GameOver == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showNumber(TopScore)
         basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -80,13 +124,15 @@ basic.forever(function () {
             basic.pause(100)
         }
         if (GameOver == 1) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
             basic.showLeds(`
                 . . # . .
-                . # . . .
+                . . . # .
                 # # # # #
-                . # . . .
+                . . . # .
                 . . # . .
                 `)
+            pins.digitalWritePin(DigitalPin.P1, 0)
             basic.showLeds(`
                 . . . . .
                 . . . . .
@@ -96,13 +142,15 @@ basic.forever(function () {
                 `)
         }
         if (GameOver == 1) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
             basic.showLeds(`
                 . . # . .
-                . . . # .
+                . # . . .
                 # # # # #
-                . . . # .
+                . # . . .
                 . . # . .
                 `)
+            pins.digitalWritePin(DigitalPin.P0, 0)
             basic.showLeds(`
                 . . . . .
                 . . . . .
